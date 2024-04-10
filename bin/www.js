fe31ac0ccb59fb9595eaf688a3726492b44297b3
SeoPPak <andrew201239@gmail.com>
2024-04-10 21:59:27 +0900

    change port as env

--- bin/www.js
+++ bin/www.js
@@ -3,6 +3,6 @@ var app = require('../app')
 
 app.set('port', 3000)
 
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(process.env.PORT || app.get('port'), function() {
   console.log('Express server listening on ' + server.address().address)
 })
